42a504fa435299f0d9c217d09a2b8362d08aa023
Flexible78 <alexandertsyrkin@gmail.com>
2023-04-02 22:17:08 +0400

    fix: modal cv page files

---
+++ dist/js/modal.js
@@ -0,0 +1,35 @@
+'uses strict';
+
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__inner');
+const modalOpenTrigger = document.querySelectorAll('[data-open-modal-cv]');
+const modalCloseTrigger = document.querySelectorAll('[data-close-modal-cv]');
+
+function openModal() {
+  modal.classList.add('modal--visible');
+  modal.classList.remove('modal--hidden');
+  document.body.style.overflowY = 'hidden';
+}
+function closeModal() {
+  modal.classList.add('modal--hidden');
+  modal.classList.remove('modal--visible');
+  document.body.style.overflowY = '';
+}
+
+modalOpenTrigger.forEach((element) => {
+  element.addEventListener('click', openModal);
+});
+
+modalCloseTrigger.forEach((element) => {
+  element.addEventListener('click', closeModal);
+});
+modal.addEventListener('click', (e) => {
+  if (e.target === modalOverlay) {
+    closeModal();
+  }
+});
+modal.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape') {
+    closeModal();
+  }
+});
