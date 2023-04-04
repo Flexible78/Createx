09ddad82362cb4f5f711e38480e95232309d5cbf
Flexible78 <alexandertsyrkin@gmail.com>
2023-04-04 23:45:08 +0400

    fix: homepage adaptive page files

--- dist/js/modal-subscribe.js
+++ dist/js/modal-subscribe.js
@@ -1,35 +1,34 @@
-'uses strict';
+'use strict';
 
-const modal = document.querySelector('.modal-subscribe');
-const modalOverlay = document.querySelector('.modal-subscribe__inner');
-const modalSubscribeopenTrigger = document.querySelectorAll('[data-open-modal-subscribe]');
-const modalSubscribeсloseTrigger = document.querySelectorAll('[data-close-modal-subscribe]');
+const modalSubcribe = document.querySelector('.modal-subscribe');
+const modalOverlaySubscribe = document.querySelector('.modal-subscribe__inner');
+const modalOpenSubcribeTrigger = document.querySelectorAll('[data-open-modal-cv]');
+const modalCloseSubcribeTrigger = document.querySelectorAll('[data-close-modal-cv]');
 
-function openModal() {
-  modal.classList.add('modal--visible');
-  modal.classList.remove('modal--hidden');
+function openModalS() {
+  modalSubcribe.classList.add('modal-subscribe--visible');
+  modalSubcribe.classList.remove('modal-subscribe--hidden');
   document.body.style.overflowY = 'hidden';
 }
-function closeModal() {
-  modal.classList.add('modal--hidden');
-  modal.classList.remove('modal--visible');
+function closeModalS() {
+  modalSubcribe.classList.add('modal-subscribe--hidden');
+  modalSubcribe.classList.remove('modal-subscribe--visible');
   document.body.style.overflowY = '';
 }
-modalSubscribeopenTrigger.forEach((element) => {
-  element.addEventListener('click', openModal);
+modalOpenSubcribeTrigger.forEach((element) => {
+  element.addEventListener('click', openModalS);
 });
 
-modalSubscribeсloseTrigger.forEach((element) => {
-  element.addEventListener('click', closeModal);
+modalCloseSubcribeTrigger.forEach((element) => {
+  element.addEventListener('click', closeModalS);
 });
-
-modal.addEventListener('click', (e) => {
-  if (e.target === modalOverlay) {
-    closeModal();
+modalSubcribe.addEventListener('click', (e) => {
+  if (e.target === modalOverlaySubscribe) {
+    closeModalS();
   }
 });
-modal.addEventListener('keydown', (e) => {
+modalSubcribe.addEventListener('keydown', (e) => {
   if (e.code === 'Escape') {
-    closeModal();
+    closeModalS();
   }
 });
